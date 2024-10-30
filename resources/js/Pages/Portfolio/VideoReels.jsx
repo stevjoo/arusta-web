ad082b8043e8df5448ba30e6bc8b9c8e76606630
stevjoo <goodjestcom@gmail.com>
2024-10-30 22:57:59 +0700

    Routing Page

---
+++ resources/js/Pages/Portfolio/VideoReels.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <>
+            <h1 className="text-blue-900 font-black text-5xl">Video Reels</h1>
+        </>
+    );
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return inertia('Home');
 });
+
+Route::get('/behindthelense', function () {
+    return inertia('BehindtheLense');
+});
+
+Route::get('/portfolio', function () {
+    return inertia('Portfolio/Portfolio');
+});
+
+Route::get('/photography', function () {
+    return inertia('Portfolio/Photography');
+});
+
+Route::get('/video-reels', function () {
+    return inertia('Portfolio/VideoReels');
+});
+
+Route::get('/graphic-design', function () {
+    return inertia('Portfolio/GraphicDesign');
+});
