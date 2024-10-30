9abaec97110a20729a3aa218df6569dfcd1a5376
stevjoo <goodjestcom@gmail.com>
2024-10-30 23:06:26 +0700

    Admin Page

---
+++ resources/js/Pages/Admin/EditHome.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <>
+            <h1 className="text-blue-900 font-black text-5xl">
+                Edit Home
+            </h1>
+        </>
+    );
+}
---
+++ resources/js/Pages/Admin/EditPortfolio/EditGraphicDesign.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <>
+            <h1 className="text-blue-900 font-black text-5xl">Edit Graphic Design</h1>
+        </>
+    );
+}
