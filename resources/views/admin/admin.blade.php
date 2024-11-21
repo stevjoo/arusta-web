64393e6cbcb94e0bd82dc99bbf7f70f970b88e8e
stevjoo <goodjestcom@gmail.com>
2024-11-22 06:30:13 +0700

    admin can't delete account

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -9,5 +9,11 @@
                 {{ __('Admin Dashboard') }}
             </h2>
         </div>
+        <div class="space-x-5">
+            <a href="/admin-behind-the-lense">Behind the Lense</a>
+            <a href="/admin-photography">Photography</a>
+            <a href="/admin-graphic-design">Graphic Design</a>
+            <a href="/admin-behind-the-lense">Behind the Lense</a>
+        </div>
     </header>
 @endsection
--- resources/views/user/behind-the-lense.blade.php
+++ resources/views/user/behind-the-lense.blade.php
@@ -1,4 +1,16 @@
+@extends('layouts.app')
+
 @section('title', 'Behind the Lense')
+
+@section('header')
+    <header class="bg-white shadow">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Behind the Lense') }}
+            </h2>
+        </div>
+    </header>
+@endsection
 
 @section('content')
 <div class="container">
@@ -13,3 +25,4 @@
         @endforeach
     </div>
 </div>
+@endsection
