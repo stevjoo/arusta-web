95b01c1c6290edf0aaca9e0796d4ad5f69cc26d8
stevjoo <goodjestcom@gmail.com>
2024-11-27 16:49:57 +0700

    Profile Page UI Done???

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -3,9 +3,9 @@
 @section('title', 'Profile')
 
 @section('header')
-    <header class="bg-white shadow">
+    <header class="bg-darkgray shadow">
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <h2 class="font-semibold text-xl text-white leading-tight">
                 {{ __('Profile Editing') }}
             </h2>
         </div>
@@ -15,20 +15,20 @@
 @section('content')
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-        <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+        <div class="p-4 sm:p-8 bg-darkgray shadow sm:rounded-lg">
             <div class="max-w-xl">
                 @include('profile.partials.update-profile-information-form')
             </div>
         </div>
 
-        <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+        <div class="p-4 sm:p-8 bg-darkgray shadow sm:rounded-lg">
             <div class="max-w-xl">
                 @include('profile.partials.update-password-form')
             </div>
         </div>
 
         @if (!auth()->user()->isAdmin())
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-darkgray shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
                 </div>
