a3e1d2984300a4acf6072f799d906ba8271fc6e3
stevjoo <goodjestcom@gmail.com>
2024-12-06 10:32:21 +0700

    Portofolio Page Frontend Done

--- resources/views/user/portfolio.blade.php
+++ resources/views/user/portfolio.blade.php
@@ -3,25 +3,25 @@
 @section('title', 'Portfolio')
 
 @section('content')
-<div>
-    <div class="text-center py-10">
-        <h1 class="text-2xl md:text-5xl tracking-[.34em] text-white font-bold">Our Portfolio</h1>
-    </div>
-    
-    <div class="container lg:flex mx-auto m-6 px-8 justify-center items-center w-3/4">
-        <a href="./photography" class="m-1 p-2 bg-gray-800 bg-opacity-100 text-gray-100 font-semibold h-72 w-full rounded-md flex items-center justify-center hover:bg-opacity-50">
-            <p class="text-center">
-                {{ __('Photography') }}
-            </p>   
+<div class="container mx-auto py-10 text-center">
+    <h1 class="text-2xl md:text-5xl tracking-[.34em] text-white font-bold mb-10">Our Portfolio</h1>
+
+    <div class="grid px-5 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
+        <!-- Photography -->
+        <a href="./photography" class="relative block rounded-lg p-2 py-96 hover:shadow-xl transition-all duration-300 ease-in-out group">
+            <h2 class="text-5xl text-white font-bold mb-4 relative z-20">Photography</h2>
+            <!-- Image with dark overlay on hover -->
+            <div class="absolute inset-0 bg-cover bg-center transition-all duration-500 ease-in-out group-hover:opacity-100 opacity-50" style="background-image: url('/asset/Photography/banner.webp');">
+            </div>
         </a>
-        <a href="./graphic-design" class="m-1 p-2 bg-gray-800 bg-opacity-100 text-gray-100 font-semibold h-72 w-full rounded-md flex items-center justify-center hover:bg-opacity-50">
-            <p class="text-center">
-                {{ __('Graphic Design') }}
-            </p>
+
+        <!-- Graphic Design -->
+        <a href="./graphic-design" class="relative block rounded-lg p-6 py-96 hover:shadow-xl transition-all duration-300 ease-in-out group">
+            <h2 class="text-5xl texwhite font-bold mb-4 relative z-20">Graphic Design</h2>
+            <!-- Image with dark overlay on hover -->
+            <div class="absolute inset-0 bg-cover bg-center transition-all duration-500 ease-in-out group-hover:opacity-100 opacity-50" style="background-image: url('/asset/GraphicDesign/banner.webp');">
+            </div>
         </a>
     </div>
 </div>
-
-
-
 @endsection
